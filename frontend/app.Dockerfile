a9d7940e6cb382f7c6dff7f0c7ce8d8bc5f01cef
Roberto Hernandez <betohr60@gmail.com>
2024-03-24 19:13:17 +0100

    feat: first workflow for frontend ui post request to backend api

--- frontend/app.Dockerfile
+++ frontend/app.Dockerfile
@@ -2,6 +2,13 @@ FROM python:3.10-bullseye
 
 RUN apt-get update --fix-missing
 RUN pip install  --no-cache-dir --upgrade pip
+
+ENV HOMEDIR=/app
+RUN mkdir -p $HOMEDIR
+
+WORKDIR $HOMEDIR
+ENV PYTHONPATH=$PYTHONPATH:/app
+
 RUN pip install poetry
 
 COPY pyproject.toml pyproject.toml
--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,13 +1,22 @@
 """ Main app frontend file."""
 
+import time
+
+import requests
 import streamlit as st
 
-st.image("/images/logo.png", use_column_width=True)
+st.set_page_config(
+    page_title="Your CV Chatbot!",
+    page_icon=":robot_face:",
+)
+
+# Logo as header
+st.image("/app/images/logo.png", use_column_width=True)
 
 # Basic UI for users
-st.title("Ask about your CV!")
-st.header("Upload a CV in PDF format and then you could ask information \
-    about it directly without checking the entire resume.")
+st.title("Ask about your CV! :briefcase: :page_with_curl:")
+st.header("Upload a CV and then you could ask information \
+        about it directly without checking the entire resume.")
 
 uploaded_cv = st.file_uploader(
     label="Upload your CV here. Only PDF formats are allowed.",
@@ -15,8 +24,28 @@ uploaded_cv = st.file_uploader(
 )
 
 if uploaded_cv:
+    with st.spinner("Reading CV. Please wait..."):
+        time.sleep(5)
+        # 1. OCR: read CV
+        # 2. Compute embeddings
+        # 3. Store embeddings in VectorDB
+        # RAG
+
     st.write("CV uploaded successfully.")
 
-user_question = st.text_area("Question:")
+st.text("")
 
-st.button("Get answer")
+user_question = st.text_area("What do you want to know?")
+
+if len(user_question) > 0:
+    ask = st.button("Get answer")
+
+    if ask:
+        question = {'question': user_question}
+        r = requests.post(
+            url="http://host.docker.internal:8000/question",
+            json=question,
+            timeout=200
+        )
+
+        st.text_area(r.content)
