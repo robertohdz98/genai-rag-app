3538c5ef97e97186de6daec411793c52cf4c30d7
Roberto Hernandez <betohr60@gmail.com>
2024-03-24 21:24:05 +0100

    feat: store uploaded cv in s3 storage bucket

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,9 +1,11 @@
 """ Main app frontend file."""
 
+import os
 import time
 
 import requests
 import streamlit as st
+from minio import Minio
 
 st.set_page_config(
     page_title="Your CV Chatbot!",
@@ -25,22 +27,40 @@ uploaded_cv = st.file_uploader(
 
 if uploaded_cv:
     with st.spinner("Reading CV. Please wait..."):
-        time.sleep(5)
+
+        MINIO_API_HOST = "http://host.docker.internal:9000"
+        ACCESS_KEY = os.getenv('MINIO_ROOT_USER', 'minio')
+        SECRET_KEY = os.getenv('MINIO_ROOT_PASSWORD', 'minio123')
+        s3_client = Minio(
+            "host.docker.internal:9000",
+            access_key=ACCESS_KEY,
+            secret_key=SECRET_KEY,
+            secure=False
+        )
+        with open(uploaded_cv.name, "wb") as f:
+            f.write(uploaded_cv.getbuffer())
+        s3_client.fput_object("cvs", uploaded_cv.name, uploaded_cv.name)
+        print(f"Filename [{uploaded_cv.name}] ploaded to S3 bucket.")
+
+        cv_s3_url = f"cvs/{uploaded_cv.name}"
+        requests.post(
+            url=f"http://host.docker.internal:8000/get_cv/{cv_s3_url}",
+            timeout=2000
+        )
         # 1. OCR: read CV
         # 2. Compute embeddings
         # 3. Store embeddings in VectorDB
         # RAG
 
-    st.write("CV uploaded successfully.")
+    st.success("CV uploaded successfully.")
 
 st.text("")
 
 user_question = st.text_area("What do you want to know?")
+ask_button = st.button("Ask CV Chatbot")
 
-if len(user_question) > 0:
-    ask = st.button("Get answer")
-
-    if ask:
+if ask_button:
+    if len(user_question) > 0:
         question = {'question': user_question}
         r = requests.post(
             url="http://host.docker.internal:8000/question",
@@ -48,4 +68,7 @@ if len(user_question) > 0:
             timeout=200
         )
 
-        st.text_area(r.content)
+        st.write(r.content)
+
+    else:
+        st.write(":warning:  Please, make a quote")
